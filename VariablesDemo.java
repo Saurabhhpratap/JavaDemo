be5632cc1ce269160bf0f74d4f536bbf384cac3e
saurabhhpratap <saurabhhpratap2777@gmail.com>
2023-02-20 01:03:42 +0530

    VarliablesDemo

---
+++ VariablesDemo.java
@@ -0,0 +1,21 @@
+
+public class VariablesDemo {
+
+	public static void main(String[] args) {
+		
+				// TODO Auto-generated method stub
+
+		int a=25;
+		System.out.println(a);
+		
+		float b = 2.5f;
+		System.out.println(b);
+		
+		char ch = 'A';
+		System.out.println(ch);
+		
+		boolean result= true;
+		System.out.println(result);
+	}
+
+}
